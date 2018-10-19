04f8b521b13573118c8ef4dc3cf333acf37e711e
hmukhta <hmukhta@ncsu.edu>
2018-10-18 22:49:47 -0400

    added gps functionality. put code in right screens

--- screens/GPSScreen.js
+++ screens/GPSScreen.js
@@ -1,38 +1,123 @@
-import React from 'react';
-import { Text, View, TouchableOpacity, Button, Image } from 'react-native';
+import React, { Component } from 'react';
+import { Platform, Text, View, StyleSheet, Button } from 'react-native';
+import { Constants, Location, Permissions } from 'expo';
 
-export default class SettingsScreen extends React.Component {
+
+export default class GPSScreen extends React.Component {
   static navigationOptions = {
     title: 'GPS',
   };
+  state = {
+    location: null,
+    errorMessage: null,
+  };
 
-  getMoviesFromApi = async () => {
-    try {
-      let response = await fetch('https://facebook.github.io/react-native/movies.json');
-      let responseJson = await response.json();
-      console.log('---------' + responseJson.movies)
-      return responseJson.movies;
-    } catch (error) {
-      console.error(error);
+  componentWillMount() {
+    if (Platform.OS === 'android' && !Constants.isDevice) {
+      this.setState({
+        errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+      });
+    } else {
+    //   this._getLocationAsync();
     }
   }
 
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+
+    let location = await Location.getCurrentPositionAsync({enableHighAccuracy:true});
+    this.setState({ location });
+  };
+
   render() {
+    // let text = 'Waiting..';
+    let text ="";
+    let lat = "";
+    let lng = "";
+    let altitude = "";
+    let accuracy = "";
+    let altAccuracy = "";
+    let heading = "";
+    let speed = "";
+    if (this.state.errorMessage) {
+      this.state.errorMessage;
+    } else if (this.state.location) {
+      text = JSON.stringify(this.state.location);
+      lat = this.state.location.coords.latitude;
+      lng = this.state.location.coords.longitude;
+      altitude = this.state.location.coords.altitude;
+      accuracy = this.state.location.coords.accuracy;
+      altAccuracy = this.state.location.coords.altitudeAccuracy;
+      heading = this.state.location.coords.heading;
+      speed = this.state.location.coords.speed;
+
+    //   lat = "Here";
+    }
+
     return (
-        <View style={{ flex: 1, alignItems: 'center' }}>
-          <Button
-            onPress={this.getMoviesFromApi}
-            title="Open Camera"
-            color="#841584"
-          />
-          <Text
-            style={{ fontSize: 18, marginBottom: 20, marginTop: 20 }}>
-            Your Picture Below
-          </Text>
-          <Image
-            style={{ height: 400, width: 400 }}
-          />
-        </View>
-      );
+        
+
+
+      <View style={styles.container}>
+        <Button
+        onPress={this._getLocationAsync}
+        title="Get Current Location"
+        color="#841584"
+        />
+
+        {/* <Text style={styles.paragraph}>
+        {text}
+        
+        </Text> */}
+        <Text style={styles.paragraph}>
+        Latitude: {lat}
+        </Text>
+
+        <Text style={styles.paragraph}>
+        Longitude: {lng}
+        </Text>
+
+        <Text style={styles.paragraph}>
+        Accuracy: {accuracy}
+        </Text>
+
+        <Text style={styles.paragraph}>
+        Altitude: {altitude}
+        </Text>
+
+        <Text style={styles.paragraph}>
+        Altitude Accuracy: {altAccuracy}
+        </Text>
+
+        <Text style={styles.paragraph}>
+        Heading: {heading}
+        </Text>
+
+        <Text style={styles.paragraph}>
+        Speed: {speed}
+        </Text>
+        
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    textAlign: 'center',
+  },
+});
--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,27 +1,39 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import { Text, View, TouchableOpacity, Button, Image } from 'react-native';
+
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
     title: 'API Call',
   };
 
+  getMoviesFromApi = async () => {
+    try {
+      let response = await fetch('https://facebook.github.io/react-native/movies.json');
+      let responseJson = await response.json();
+      console.log('---------' + responseJson.movies)
+      return responseJson.movies;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   render() {
     return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
-      </ScrollView>
-    );
+        <View style={{ flex: 1, alignItems: 'center' }}>
+          <Button
+            onPress={this.getMoviesFromApi}
+            title="Open Camera"
+            color="#841584"
+          />
+          <Text
+            style={{ fontSize: 18, marginBottom: 20, marginTop: 20 }}>
+            Your Picture Below
+          </Text>
+          <Image
+            style={{ height: 400, width: 400 }}
+          />
+        </View>
+      );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
-  },
-});
